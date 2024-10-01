4cbf7832a3b1d2861d02ef489ac3a663b673c23c
kotran222 <kobetrankhoi222@gmail.com>
2024-09-30 22:04:29 -0500

    fix: update baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://kotran222.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/elastic-stack-notes/",
+  baseUrl: "/elasticstack-notes/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
